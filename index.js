c1efd81cb71e9e019c9eb31ff21a18cf0ec304fc
Kalin Hristov <kalin0169@gmail.com>
2023-10-02 10:38:22 +0300

    Server is running on Port 3030!

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('./middlewares/cors');
+const app = express();
+
+app.use(express.json());
+// Allow the Server to receive Requests from *
+// CORS(Errors)
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'REST service operational' });
+});
+
+app.listen(3030, () => console.log('REST service started'));
