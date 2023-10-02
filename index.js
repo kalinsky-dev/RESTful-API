a2d5b5192dc5026bef50521696fd6c21b67306f6
Kalin Hristov <kalin0169@gmail.com>
2023-10-02 11:39:56 +0300

    Connected to the MongoDB!

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const cors = require('./middlewares/cors');
-const app = express();
 
-app.use(express.json());
+const connectionString = 'mongodb://127.0.0.1:27017/furniture';
 
-// Allow the Server to receive Requests from *
-// CORS(Errors)
-app.use(cors());
+start();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'REST service operational' });
-});
+async function start() {
+  await mongoose.connect(connectionString);
+  console.log('Database connected!');
 
-app.listen(3030, () => console.log('REST service started'));
+  const app = express();
+
+  app.use(express.json());
+  // Allow the Server to receive Requests from *
+  // CORS(Errors)
+  app.use(cors());
+
+  app.get('/', (req, res) => {
+    res.json({ message: 'REST service operational' });
+  });
+
+  app.listen(3030, () => console.log('REST service started'));
+}
